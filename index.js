13cb2075694e8e39bbda515977114e80a7da9d8f
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-10 15:52:36 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN >= toN) {
+    return fromN;
+  }
+
+  let result = fromN + sum(fromN + 1, toN);
+
+  return result;
 }
 
 module.exports = sum;
